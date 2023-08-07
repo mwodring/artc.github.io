170f0c9711be6d3f00be74b850e4e9ce2832cfb5
mwodring <Morgan.Wodring@fera.co.uk>
2023-08-07 18:24:58 +0100

    Still messing with toggle-able table of contents

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -5,10 +5,17 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
+    var children = content.childNodes;
+    if (content.style.display === "inline-block") {
       content.style.display = "none";
+      for (i = 0; i < children.length; i++) {
+          children[i].style.display = "none";
+      }
     } else {
       content.style.display = "inline-block";
+      for (i = 0; i < children.length; i++) {
+          children[i].style.display = "inline-block";
+      }
     }
   });
 }
