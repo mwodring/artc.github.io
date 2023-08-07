4398e429849a7f7b14aea13dc33acfb5dc6d41ef
mwodring <Morgan.Wodring@fera.co.uk>
2023-08-07 20:00:42 +0100

    Signed-off-by: mwodring <Morgan.Wodring@fera.co.uk>

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -4,9 +4,9 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var par = this.parentNode;
+    var par = this.parentNode.parentNode;
     var children = par.children;
-    for (var i =0; i < content.length; i++) {
+    for (var i =0; i < children.length; i++) {
     var content = children[i];
     if (content.style.display === "block") {
       content.style.display = "none";
