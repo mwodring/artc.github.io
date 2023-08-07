d040d965e9a5bb3b4d1dc9c23dcf20f61b0fe5a3
mwodring <Morgan.Wodring@fera.co.uk>
2023-08-07 14:55:52 +0100

    space delimited

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -5,10 +5,12 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.childNodes;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
+    for(var i = 0, size = content.length; i < size ; i++){
+      if (content[i]].style.display === "block") {
+        content[i].style.display = "none";
+      } else {
+        content[i].style.display = "block";
+      }
     }
   });
 }
