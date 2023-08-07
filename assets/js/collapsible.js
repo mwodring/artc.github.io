84442c988eb455b84c6df57d49bbf236d4323303
mwodring <Morgan.Wodring@fera.co.uk>
2023-08-07 14:47:40 +0100

    Trying childNodes instead

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -4,7 +4,7 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    var content = this.childNodes;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
