903e5144ae13c20b1b79a2b84f96cd80d70e51f9
mwodring <Morgan.Wodring@fera.co.uk>
2023-08-07 15:43:35 +0100

    Changed toc.html?

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -4,13 +4,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.childNodes;
-    for(var i = 0, size = content.length; i < size ; i++){
-      if (content[i].style.display === "block") {
-        content[i].style.display = "none";
-      } else {
-        content[i].style.display = "block";
-      }
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
     }
   });
 }
