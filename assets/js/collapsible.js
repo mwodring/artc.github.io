d317160840955158ce9251c48042ab5f444a6641
mwodring <Morgan.Wodring@fera.co.uk>
2023-08-07 18:38:02 +0100

    Signed-off-by: mwodring <Morgan.Wodring@fera.co.uk>

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -5,17 +5,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    var children = content.childNodes;
     if (content.style.display === "inline-block") {
       content.style.display = "none";
-      for (i = 0; i < children.length; i++) {
-          children[i].style.display = "none";
-      }
     } else {
       content.style.display = "inline-block";
-      for (i = 0; i < children.length; i++) {
-          children[i].style.display = "inline-block";
-      }
     }
   });
 }
